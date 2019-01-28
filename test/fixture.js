935a04bef8f4609536fe6a08976fbb536f1b0320
Harris Schneiderman <schne324@gmail.com>
2019-01-27 17:25:52 -0800

    initial commit - can be used as a backup for a 'starter' (test cases written and fixture created)

---
+++ test/fixture.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = `
+    <div role="tablist" aria-label="CSUN">
+        <button
+            role="tab"
+            aria-selected="true"
+            aria-controls="csun17-panel"
+            id="csun17">
+            CSUN 17
+        </button>
+        <button
+            role="tab"
+            aria-selected="false"
+            aria-controls="csun18-panel"
+            id="csun18">
+            CSUN 18
+        </button>
+        <button
+            role="tab"
+            aria-selected="false"
+            aria-controls="csun19-panel"
+            id="csun19">
+            CSUN 19
+        </button>
+    </div>
+    <div role="tabpanel" id="csun17-panel" aria-labelledby="csun17">
+        <p>CSUN 2017 was my first CSUN and my talk was titled "Advanced ARIA"</p>
+        <a href="https://schne324.github.io/csun-advanced-aria/" target="_blank">
+            View the csun17 slides here
+        </a>
+    </div>
+    <div role="tabpanel" id="csun18-panel" aria-labelledby="csun18">
+        <p>At CSUN 2018, my coworker and I presented "Nightmare on HTML Street"</p>
+        <a href="https://schne324.github.io/csun-combobo-talk-slides/" target="_blank">
+            View the csun18 slides here
+        </a>
+    </div>
+    <div role="tabpanel" id="csun19-panel" aria-labelledby="csun19">
+        <p>At CSUN 2019 (right now!), I did some live coding...and it was a DISASTER (kidding hopefully)</p>
+    </div>
+`;
