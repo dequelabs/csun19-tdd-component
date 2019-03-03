a057e25733c090f26682333821c860b04969f785
Harris Schneiderman <harris.schneiderman@deque.com>
2019-03-03 08:17:29 -0800

    Phase 3 (fixed): tests running -- all failures

--- test/index.js
+++ test/index.js
@@ -14,19 +14,19 @@ let tablist, tabs, panels;
 const fixture = document.createElement('div');
 // Before each test runs we setup a "new"
 // fixture and call csunTabs on the tablist
-test.beforeEach(t => {
+test.beforeEach(() => {
   fixture.innerHTML = html;
-  tablist = fixture.querySelector('[role="tablist"]') 
+  tablist = fixture.querySelector('[role="tablist"]')
   tabs = [...fixture.querySelectorAll('[role="tab"]')];
   panels = [...fixture.querySelectorAll('[role="tabpanel"]')];
   csunTabs(tablist);
 });
 // after each test clear out the fixture
-test.afterEach(t => fixture.innerHTML = '');
+test.afterEach(() => fixture.innerHTML = '');
 
 /**
  * Tab: When focus moves into the tab list, places focus on the active tab element . When the tab list contains the focus, moves focus to the next element in the page tab sequence outside the tablist, which is typically either the first focusable element inside the tab panel or the tab panel itself.
- * 
+ *
  * - this means we need to manage tabIndex (0 on the "active" tab and -1 on the rest)
  */
 test('manages tab index', t => {
@@ -38,10 +38,11 @@ test('manages tab index', t => {
 
 /**
  * Left Arrow: moves focus to the previous tab. If focus is on the first tab, moves focus to the last tab.
- * 
+ *
  * - circular!
  */
 test('given a left arrow, focuses the previous tab', t => {
+  const [ tab1, tab2, tab3 ] = tabs;
   const arrowLeft = simulant('keydown', { key: 'ArrowLeft' });
   simulant.fire(tab1, arrowLeft);
   t.is(document.activeElement, tab3);
@@ -51,10 +52,11 @@ test('given a left arrow, focuses the previous tab', t => {
 
 /**
  * Right Arrow: Moves focus to the next tab. If focus is on the last tab element, moves focus to the first tab.
- * 
+ *
  * - circular!
  */
 test('given a right arrow, focuses the next tab', t => {
+  const [tab1, tab2, tab3] = tabs;
   const arrowRight = simulant('keydown', { key: 'ArrowRight' });
   simulant.fire(tab3, arrowRight);
   t.is(document.activeElement, tab1);
@@ -81,7 +83,7 @@ test('role=tab is set on each tab', t => {
 
 /**
  * If the tab list has a visible label, the element with role tablist has aria-labelledby set to a value that refers to the labeling element. Otherwise, the tablist element has a label provided by aria-label.
- * 
+ *
  * - check for aria-labelledby OR aria-label
  */
 test('the tablist has an accessible label', t => {
